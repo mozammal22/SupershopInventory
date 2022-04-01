d0f53c1d4a15d05af93d49e5a0257e6af8954024
Md. Mozammal Hosen <mozammal984@gmail.com>
2022-04-01 22:58:13 +0600

    Add project files.

---
+++ Inventory.Core/Entity/Base/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Core.Entity.Base
+{
+    public class BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime ModifiedTime { get; set; }
+    }
+}
---
+++ Inventory.Core/Entity/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Inventory.Core.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Core.Entity
+{
+    public class Customer : BaseModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
