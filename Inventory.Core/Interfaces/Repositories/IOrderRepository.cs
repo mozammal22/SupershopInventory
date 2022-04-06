160584576c5ae41a817a90bd4b65ce582bc58ae6
Md. Mozammal Hosen <mozammal984@gmail.com>
2022-04-07 00:28:15 +0600

    Create all interface

---
+++ Inventory.Core/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Inventory.Core.Entity;
+using Inventory.Core.Interfaces.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Core.Interfaces.Repositories
+{
+    public interface IOrderRepository:IBaseRepository<Order>
+    {
+    }
+}
