dc6a64ca121fd6520d4d37aa3ce4a9f2be2b7a6c
Md. Mozammal Hosen <mozammal984@gmail.com>
2022-04-08 00:38:06 +0600

    Data layer and business layer

---
+++ Inventory.Business/Repositories/BaseRepository.cs
@@ -0,0 +1,74 @@
+﻿using Inventory.Core.Entity.Base;
+using Inventory.Core.Interfaces.Repositories.Base;
+using Inventory.Data.InventoryContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Business.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseModel
+    {
+        private readonly InventoryDbContext context;
+
+        public BaseRepository(InventoryDbContext _context)
+        {
+            context = _context;
+        }
+        public void Add(T entity)
+        {
+            entity.CreatedTime = DateTime.Now;
+            context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(List<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            entity.CreatedTime = DateTime.Now;
+            context.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(List<T> entites)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(int id)
+        {
+            return GetAll().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            entity.CreatedTime = DateTime.Now;
+            context.Set<T>().Update(entity);
+        }
+
+        public void UpdateRange(List<T> entites)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Inventory.Data/InventoryContext/InventoryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Inventory.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data.InventoryContext
+{
+    public class InventoryDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+
+}
+
